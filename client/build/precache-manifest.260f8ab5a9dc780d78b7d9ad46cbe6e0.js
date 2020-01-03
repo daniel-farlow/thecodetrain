a1b3b4b085b4fc52c8a0b91e69abc9871b78fbbb
Daniel Farlow <dan.farlow@gmail.com>
2020-01-03 12:28:19 -0600

    build for production

---
+++ client/build/precache-manifest.260f8ab5a9dc780d78b7d9ad46cbe6e0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1c41b65285d856c45f56d9fdddc1331",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f670709aff0647e672e8",
+    "url": "/static/css/2.7397835a.chunk.css"
+  },
+  {
+    "revision": "92f3bb3d33fb02470c03",
+    "url": "/static/css/main.7735413d.chunk.css"
+  },
+  {
+    "revision": "f670709aff0647e672e8",
+    "url": "/static/js/2.3add545a.chunk.js"
+  },
+  {
+    "revision": "62fe1e4ea221af446b54c45afe9a16c4",
+    "url": "/static/js/2.3add545a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "92f3bb3d33fb02470c03",
+    "url": "/static/js/main.fc316996.chunk.js"
+  },
+  {
+    "revision": "2bcb8084869f23f72706",
+    "url": "/static/js/runtime-main.88d4f3f8.js"
+  }
+]);
